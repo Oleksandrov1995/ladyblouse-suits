c9fcff753454ed0f476f05cd9494b47d02729731
Oleksandrov1995 <olexandrov1995@gmail.com>
2024-06-11 20:41:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="ladyblouse-suits/">
+    <BrowserRouter basename="/ladyblouse-suits">
       <App />
     </BrowserRouter>
   </React.StrictMode>
